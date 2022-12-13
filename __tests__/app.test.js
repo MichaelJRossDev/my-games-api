3db6fd5b4136669a12504228edf236e750a900c6
MichaelJRossDev <michaeljrossdev@gmail.com>
2022-12-13 14:36:26 +0000

    Refactor and handle internal server errors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,11 +2,16 @@ const request = require('supertest')
 const app = require('../app')
 const data = require('../db/data/test-data/index')
 const seed = require('../db/seeds/seed')
+const db = require('../db/connection')
 
 const categoriesData = require('../db/data/test-data/categories')
 
 beforeEach(() => {
     return seed(data);
+})
+
+afterAll(() => {
+    db.end();
 })
 
 describe('GET Categories', () => {
@@ -15,16 +20,11 @@ describe('GET Categories', () => {
         .expect(200)
         .then((res) => {
             const categories = res.body.categories;
-
-            console.log(categories);
-
-            expect(Array.isArray(categories)).toBe(true);
             expect(categories.length).toBe(4);
             
             categories.forEach(category => {
                 expect(typeof category.slug).toBe('string');
                 expect(typeof category.description).toBe('string');
-                expect(Object.keys(category).length).toBe(2);
             });
         })
     });
--- controllers/categories.js
+++ controllers/categories.js
@@ -1,5 +1,7 @@
+const { reduce } = require('../db/data/test-data/categories');
 const { selectCategories } = require('../models/categories');
 
 exports.sendCategories = (req, res) => {
-    selectCategories().then((categories) => res.status(200).send({ categories }));
+    selectCategories().then((categories) => res.status(200).send({ categories }))
+    .catch((err) => res.sendStatus(500));
 };
