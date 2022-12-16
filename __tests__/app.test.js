e7f7bccdc8ded6f609611198b887235a260f2b97
MichaelJRossDev <michaeljrossdev@gmail.com>
2022-12-16 15:21:56 +0000

    Revert "Merge pull request #9 from MichaelJRossDev/6-GET-/api/reviews/review_id/comments"
    This reverts commit 714d680e38d5ec2584e11107de6a630300a2b9cf, reversing
    changes made to ec7431e87733c3f0c3d22535f5c492fd2d7b1263.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -58,27 +58,6 @@ describe('GET Categories', () => {
     });
 });
 
-
-describe('GET comments by review ID', () => {
-    test('Should return all comments of a given review', () => {
-        return request(app).get('/api/reviews/3/comments')
-        .expect(200)
-        .then((response) => {
-            const comments = response.body.comments;
-            expect(comments.length).not.toBe(0);
-            comments.forEach(comment => {
-                expect(comment).toHaveProperty('comment_id');
-                expect(comment).toHaveProperty('body');
-                expect(comment.review_id).toBe(3);
-                expect(comment).toHaveProperty('author');
-                expect(comment).toHaveProperty('votes');
-                expect(comment).toHaveProperty('created_at');
-            });
-
-            expect(comments).toBeSortedBy('created_at', {descending : true});
-    })
-})
-
 describe('POST Comment', () => {
     const correctComment = {
         username: "philippaclaire9",
--- models/comments.js
+++ models/comments.js
@@ -1,14 +1,5 @@
 const db = require("../db/connection.js");
 
-
-exports.selectCommentsByReviewId = (id) => {
-    return db.query(`
-    SELECT * FROM comments 
-    WHERE review_id = $1
-    ORDER BY created_at DESC`, [id]).then((result) => result.rows)
-    .catch(err => err);
-}
-
 exports.postComment = (id, payload) => {
     return db.query(`
     INSERT INTO comments (body, review_id, author, votes)
